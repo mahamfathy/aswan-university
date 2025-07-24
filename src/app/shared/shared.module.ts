75be0a74182953e53d8d2f011d9ca0be49239b03
mahamfathy <mahafathyh@gmail.com>
2025-07-24 23:00:46 +0300

    [feat] module : add shared module

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,100 @@
+import { CommonModule, NgFor } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import {
+  MatDialogActions,
+  MatDialogClose,
+  MatDialogContent,
+  MatDialogModule,
+  MatDialogTitle,
+} from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTableModule } from '@angular/material/table';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterModule } from '@angular/router';
+// import { NgxDropzoneModule } from 'ngx-dropzone';
+import { DeleteItemComponent } from './components/delete-item/delete-item.component';
+import { TableComponent } from './components/table/table.component';
+@NgModule({
+  declarations: [
+    TableComponent,
+    DeleteItemComponent,
+    // FooterComponent,
+    // UserNavbarComponent,
+  ],
+  imports: [
+    CommonModule,
+    RouterModule,
+    MatPaginatorModule,
+    MatButtonModule,
+    MatMenuModule,
+    MatIconModule,
+    MatCardModule,
+    FormsModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    ReactiveFormsModule,
+    MatProgressSpinnerModule,
+    MatToolbarModule,
+    MatListModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatChipsModule,
+    NgFor,
+    MatDialogActions,
+    MatDialogClose,
+    MatDialogContent,
+    MatDialogTitle,
+    // NgxDropzoneModule,
+    MatDatepickerModule,
+    MatSlideToggleModule,
+  ],
+  exports: [
+    RouterModule,
+    MatPaginatorModule,
+    MatButtonModule,
+    MatMenuModule,
+    MatIconModule,
+    MatCardModule,
+    FormsModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    ReactiveFormsModule,
+    MatProgressSpinnerModule,
+    MatToolbarModule,
+    MatListModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    TableComponent,
+    MatDialogActions,
+    MatDialogClose,
+    MatDialogContent,
+    MatDialogTitle,
+    // NgxDropzoneModule,
+    // FooterComponent,
+    MatDatepickerModule,
+    MatSlideToggleModule,
+    MatSlideToggleModule,
+    // UserNavbarComponent,
+  ],
+})
+export class SharedModule {}
