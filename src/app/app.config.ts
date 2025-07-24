7fe0ae1d82782c1af6ba4fd5711d9a2d2abbcb5e
mahamfathy <mahafathyh@gmail.com>
2025-07-24 21:13:16 +0300

    [feat]  app-config : add needed providers

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,15 @@
 import { ApplicationConfig } from '@angular/core';
+import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideRouter } from '@angular/router';
 
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideToastr } from 'ngx-toastr';
 import { routes } from './app.routes';
-
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    provideAnimations(),
+    provideAnimationsAsync(),
+    provideToastr(),
+  ],
 };
