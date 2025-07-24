8183b286b583a7566d6677713df0738f20fd8cca
mahamfathy <mahafathyh@gmail.com>
2025-07-24 21:13:48 +0300

    [feat]  app-routes : add needed routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () =>
+      import('./features/dashboard/modules/student/student.module').then(
+        (m) => m.StudentModule
+      ),
+  },
+  {
+    path: 'auth',
+    loadChildren: () =>
+      import('./features/auth/auth.module').then((m) => m.AuthModule),
+  },
+  {
+    path: 'admin',
+    // canActivate: [adminGuard],
+    loadChildren: () =>
+      import('./features/dashboard/modules/admin/admin.module').then(
+        (m) => m.AdminModule
+      ),
+  },
+];
