e3eea450bfa88c17303c526f44feff9734bc119c
mahamfathy <mahafathyh@gmail.com>
2025-07-27 21:53:11 +0300

    [feat] component : finish sdgs component

--- src/app/features/dashboard/modules/student/components/sdgs/sdgs.component.ts
+++ src/app/features/dashboard/modules/student/components/sdgs/sdgs.component.ts
@@ -6,4 +6,18 @@ import { Component } from '@angular/core';
   templateUrl: './sdgs.component.html',
   styleUrl: './sdgs.component.scss',
 })
-export class SdgsComponent {}
+export class SdgsComponent {
+  header: string = 'assets/images/png/sdgs/thumbnail_sdg-header-ar.png';
+  sdgsImages: string[] = [
+    'assets/images/png/sdgs/img1.png',
+    'assets/images/png/sdgs/img2.png',
+    'assets/images/png/sdgs/img3.png',
+    'assets/images/png/sdgs/img4.png',
+    'assets/images/png/sdgs/img5.png',
+    'assets/images/png/sdgs/img6.png',
+    'assets/images/png/sdgs/img7.png',
+    'assets/images/png/sdgs/img8.png',
+    'assets/images/png/sdgs/img9.png',
+    'assets/images/png/sdgs/img10.png',
+  ];
+}
