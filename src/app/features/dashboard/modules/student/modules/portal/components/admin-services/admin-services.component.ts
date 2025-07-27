085a7b9cbcf5fd3afe9490932ab299d112aeb806
mahamfathy <mahafathyh@gmail.com>
2025-07-27 19:24:24 +0300

    [feat] component :  create admin component in student module

---
+++ src/app/features/dashboard/modules/student/modules/portal/components/admin-services/admin-services.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-services',
+  standalone: false,
+  templateUrl: './admin-services.component.html',
+  styleUrls: ['./admin-services.component.scss'],
+})
+export class AdminServicesComponent {}
