96030c65909d07da4e9f6810ef96e38958c3a7f5
mahamfathy <mahafathyh@gmail.com>
2025-07-27 19:25:09 +0300

    [feat] component :  create medical servcies component in student module

---
+++ src/app/features/dashboard/modules/student/modules/portal/components/medical-services/medical-services.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-medical-services',
+  standalone: false,
+  templateUrl: './medical-services.component.html',
+  styleUrls: ['./medical-services.component.scss'],
+})
+export class MedicalServicesComponent {}
