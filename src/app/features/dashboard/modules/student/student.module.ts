fe7338d45df0460ceaa4e57fbb9e306d98bd7d6b
mahamfathy <mahafathyh@gmail.com>
2025-07-28 22:37:41 +0300

    [feat] module:  edits in student module

--- src/app/features/dashboard/modules/student/student.module.ts
+++ src/app/features/dashboard/modules/student/student.module.ts
@@ -6,13 +6,17 @@ import { NgxPaginationModule } from 'ngx-pagination';
 // import { LoginRegisterDialogComponent } from './home/components/login-register-dialog/login-register-dialog.component';
 import { NgOptimizedImage } from '@angular/common';
 import { SharedModule } from '../../../../shared/shared.module';
+import { SdgsComponent } from './components/sdgs/sdgs.component';
+import { ServicesComponent } from './components/services/services.component';
+import { SingleServiceComponent } from './components/services/single-service/single-service.component';
 import { StudentRoutingModule } from './student-routing.module';
 import { StudentComponent } from './student.component';
 @NgModule({
   declarations: [
     StudentComponent,
-    // LoginRegisterDialogComponent,
-    // FavRoomsComponent,
+    SdgsComponent,
+    ServicesComponent,
+    SingleServiceComponent,
   ],
   imports: [
     CommonModule,
