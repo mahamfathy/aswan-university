9ae92bb798068d508b07d4bbe800c2a9a9a8dcb3
mahamfathy <mahafathyh@gmail.com>
2025-07-28 22:37:11 +0300

    [feat] component :  create services component

---
+++ src/app/features/dashboard/modules/student/components/services/services.component.ts
@@ -0,0 +1,57 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-services',
+  standalone: false,
+  templateUrl: './services.component.html',
+  styleUrls: ['./services.component.scss'],
+})
+export class ServicesComponent {
+  services = [
+    {
+      title: 'خدمات طلاب',
+      image: 'assets/images/jpg/services/img1.jpg',
+      route: '/services/students-services',
+    },
+    {
+      title: 'خدمات إلكترونية',
+      image: 'assets/images/jpg/services/img2.jpg',
+      route: '/services/electronic-services',
+    },
+    {
+      title: 'خدمات مجتمع',
+      image: 'assets/images/jpg/services/img3.jpg',
+      route: '/services/community-services',
+    },
+    {
+      title: 'خدمات طبية',
+      image: 'assets/images/jpg/services/img4.jpg',
+      route: '/services/medical-services',
+    },
+    {
+      title: 'خدمات لوجستية',
+      image: 'assets/images/jpg/services/img5.jpg',
+      route: '/services/logistic-services',
+    },
+    {
+      title: 'خدمات خريجين',
+      image: 'assets/images/jpg/services/img6.jpg',
+      route: 'graduates-services',
+    },
+    {
+      title: 'خدمات هيئة أعضاء التدريس',
+      image: 'assets/images/jpg/services/img7.jpg',
+      route: '/services/faculty-services',
+    },
+    {
+      title: 'مرافق',
+      image: 'assets/images/jpg/services/img8.jpg',
+      route: '/services/facilities',
+    },
+  ];
+  constructor(private router: Router) {}
+  navigateToService(route: string): void {
+    this.router.navigate([route]);
+  }
+}
