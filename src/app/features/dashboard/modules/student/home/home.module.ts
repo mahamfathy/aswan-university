635b1e7935a4e3fb6447bb1d9a85194fe507b67f
mahamfathy <mahafathyh@gmail.com>
2025-07-28 22:39:41 +0300

    [feat] module:  edits in home module

--- src/app/features/dashboard/modules/student/home/home.module.ts
+++ src/app/features/dashboard/modules/student/home/home.module.ts
@@ -4,18 +4,36 @@ import { MatStepperModule } from '@angular/material/stepper';
 // import { TranslateModule } from '@ngx-translate/core';
 // import { QuillModule } from 'ngx-quill';
 // import { NgxStripeModule } from 'ngx-stripe';
+import { NgOptimizedImage } from '@angular/common';
+import { CarouselModule } from 'ngx-owl-carousel-o';
 import { SharedModule } from '../../../../../shared/shared.module';
+import { CollegesComponent } from './components/colleges/colleges.component';
+import { NewsAnnouncementComponent } from './components/news-announcement/news-announcement.component';
+import { SharedCardComponent } from './components/news-announcement/shared-card/shared-card.component';
+import { NumbersComponent } from './components/numbers/numbers.component';
+import { RankingComponent } from './components/ranking/ranking.component';
+import { VideosComponent } from './components/videos/videos.component';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
-
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [
+    HomeComponent,
+    NewsAnnouncementComponent,
+    RankingComponent,
+    NumbersComponent,
+    VideosComponent,
+    CollegesComponent,
+  ],
   imports: [
     CommonModule,
     HomeRoutingModule,
     // TranslateModule,
+    NgOptimizedImage,
+    CommonModule,
+    CarouselModule,
     SharedModule,
     MatStepperModule,
+    SharedCardComponent,
   ],
   exports: [],
 })
