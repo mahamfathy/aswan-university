80ec7dadce1789e3a06d540923f56b7298e3eff1
mahamfathy <mahafathyh@gmail.com>
2025-07-30 01:52:44 +0300

    [feat] enum: add auth routes enum

--- src/app/features/auth/routes/auth-routes-enum.ts
+++ src/app/features/auth/routes/auth-routes-enum.ts
@@ -1,7 +1,4 @@
 export enum authRoutes {
   LOGIN = '/auth/login',
   REGISTER = '/auth/register',
-  RESET_REQUEST_PASSWORD = '/auth/request-reset-password',
-  RESET_PASSWORD = '/auth/reset-password',
-  CHANGE_PASSWORD = '/auth/change-password',
 }
