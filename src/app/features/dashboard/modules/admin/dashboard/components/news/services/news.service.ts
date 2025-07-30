cb1398f5b69512bf0f1a609c8394d2a7d4724889
mahamfathy <mahafathyh@gmail.com>
2025-07-30 20:31:16 +0300

    [feat] service: create news service

---
+++ src/app/features/dashboard/modules/admin/dashboard/components/news/services/news.service.ts
@@ -0,0 +1,14 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { NewsItem } from '../interfaces/news-item.interface';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class NewsService {
+  private apiUrl = 'http://localhost:3000/news';
+  constructor(private http: HttpClient) {}
+  getNews() {
+    return this.http.get<NewsItem[]>(this.apiUrl);
+  }
+}
