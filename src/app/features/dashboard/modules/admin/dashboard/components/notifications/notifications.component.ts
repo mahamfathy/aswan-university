e9c0493fbe1cfed8d516c643e1e17434096d08fc
mahamfathy <mahafathyh@gmail.com>
2025-07-30 03:33:55 +0300

    [feat] component: finish notifications component

--- src/app/features/dashboard/modules/admin/dashboard/components/notifications/notifications.component.ts
+++ src/app/features/dashboard/modules/admin/dashboard/components/notifications/notifications.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { SharedModule } from '../../../../../../../shared/shared.module';
 
 @Component({
   selector: 'app-notifications',
   standalone: true,
-  imports: [],
+  imports: [SharedModule],
   templateUrl: './notifications.component.html',
-  styleUrl: './notifications.component.scss'
+  styleUrl: './notifications.component.scss',
 })
 export class NotificationsComponent {
-
+  notifications = [
+    {
+      title: 'موعد الامتحان النهائي',
+      message: 'الامتحان النهائي لكلية الهندسة سيكون يوم 12 أغسطس.',
+      type: 'info',
+    },
+    {
+      title: 'تنبيه إداري',
+      message: 'يرجى تحديث بيانات الطالب قبل نهاية الشهر.',
+      type: 'warning',
+    },
+    {
+      title: 'تهنئة 🎉',
+      message: 'تمت ترقية الأستاذة Maha إلى درجة أستاذ مشارك!',
+      type: 'success',
+    },
+  ];
 }
