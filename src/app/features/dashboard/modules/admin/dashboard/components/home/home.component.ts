2866faad0828fa0d7c439bccdffb853a20889868
mahamfathy <mahafathyh@gmail.com>
2025-07-30 03:34:43 +0300

    [feat] component: finish home component

--- src/app/features/dashboard/modules/admin/dashboard/components/home/home.component.ts
+++ src/app/features/dashboard/modules/admin/dashboard/components/home/home.component.ts
@@ -1,96 +1,47 @@
-import { Component, OnInit } from '@angular/core';
-// import {
-//   ChartConfiguration,
-//   ChartOptions,
-//   ChartType,
-// } from 'chart.js/dist/types/index';
-import { RouterModule } from '@angular/router';
-import { ToastrService } from 'ngx-toastr';
-import { Charts, Data } from './interfaces/charts';
-import { HomeService } from './services/home.service';
+import { Component } from '@angular/core';
+import { ChartData } from 'chart.js';
+import { NgChartsModule } from 'ng2-charts';
+import { SharedModule } from '../../../../../../../shared/shared.module';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterModule],
+  imports: [NgChartsModule, SharedModule],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent implements OnInit {
-  // bookingChartData = signal<ChartConfiguration['data']>({
-  //   labels: ['Pending', 'Completed'],
-  //   datasets: [
-  //     {
-  //       label: 'Bookings',
-  //       data: [],
-  //       backgroundColor: ['rgb(54, 162, 235)', 'rgb(255, 99, 132)'],
-  //       hoverOffset: 4,
-  //     },
-  //   ],
-  // });
-  // userChartData = signal<ChartConfiguration['data']>({
-  //   labels: ['Admin', 'User'],
-  //   datasets: [
-  //     {
-  //       label: 'Users',
-  //       data: [],
-  //       backgroundColor: ['rgb(75, 192, 192)', 'rgb(153, 102, 255)'],
-  //       hoverOffset: 4,
-  //     },
-  //   ],
-  // });
-  // chartType: ChartType = 'doughnut';
-  // userChartType: ChartType = 'doughnut';
-  // chartOptions: ChartOptions = { responsive: true };
-  // cards = signal<ICard[]>([]);
-
-  constructor(
-    private _HomeService: HomeService,
-    private toast: ToastrService
-  ) {}
-
-  ngOnInit(): void {
-    this._HomeService.getChartData().subscribe({
-      next: (res: Charts) => {
-        this.updateDataInCharts(res.data);
+export class HomeComponent {
+  public studentsChartData: ChartData<'doughnut'> = {
+    labels: ['الطب', 'الهندسة', 'العلوم', 'الآداب', 'التجارة'],
+    datasets: [
+      {
+        data: [1200, 800, 600, 400, 300],
+        backgroundColor: [
+          '#4bc0c0',
+          '#36a2eb',
+          '#9966ff',
+          '#ffcd56',
+          '#ff6384',
+        ],
+        hoverOffset: 10,
       },
-      error: (err) => {
-        // console.log(err);
-        this.toast.error(err.error.message);
-      },
-    });
-  }
+    ],
+  };
 
-  updateDataInCharts(res: Data) {
-    // this.cards.set([
-    //   { name: 'Rooms', number: res.rooms },
-    //   {
-    //     name: 'Facilities',
-    //     number: res.facilities,
-    //   },
-    //   { name: 'Ads', number: res.ads },
-    // ]);
-    // this.bookingChartData.set({
-    //   labels: ['Pending', 'Completed'],
-    //   datasets: [
-    //     {
-    //       label: 'Bookings',
-    //       data: [res.bookings.pending, res.bookings.completed],
-    //       backgroundColor: ['rgb(54, 162, 235)', 'rgb(255, 99, 132)'],
-    //       hoverOffset: 4,
-    //     },
-    //   ],
-    // });
-    //   this.userChartData.set({
-    //     labels: ['Admin', 'User'],
-    //     datasets: [
-    //       {
-    //         label: 'Users',
-    //         data: [res.users.admin, res.users.user],
-    //         backgroundColor: ['rgb(75, 192, 192)', 'rgb(153, 102, 255)'],
-    //         hoverOffset: 4,
-    //       },
-    //     ],
-    //   });
-  }
+  public professorsChartData: ChartData<'bar'> = {
+    labels: ['الطب', 'الهندسة', 'العلوم', 'الآداب', 'التجارة'],
+    datasets: [
+      {
+        label: 'عدد الأساتذة',
+        data: [150, 100, 80, 50, 40],
+        backgroundColor: [
+          '#60A5FA',
+          '#686f92ff',
+          '#bea953ff',
+          '#8e5ea2',
+          '#f9a826',
+        ],
+      },
+    ],
+  };
 }
