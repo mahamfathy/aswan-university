782e4f0609662879cc6751f945bf559de8735416
mahamfathy <mahafathyh@gmail.com>
2025-07-30 20:32:00 +0300

    [feat] service: create  get news method in news service

--- src/app/features/dashboard/modules/admin/dashboard/components/news/services/news.service.ts
+++ src/app/features/dashboard/modules/admin/dashboard/components/news/services/news.service.ts
@@ -1,6 +1,5 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { NewsItem } from '../interfaces/news-item.interface';
 
 @Injectable({
   providedIn: 'root',
@@ -8,7 +7,4 @@ import { NewsItem } from '../interfaces/news-item.interface';
 export class NewsService {
   private apiUrl = 'http://localhost:3000/news';
   constructor(private http: HttpClient) {}
-  getNews() {
-    return this.http.get<NewsItem[]>(this.apiUrl);
-  }
 }
