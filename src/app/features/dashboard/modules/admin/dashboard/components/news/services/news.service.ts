a77168f1f6855f95abbf60fb5f3b0c2840614b2b
mahamfathy <mahafathyh@gmail.com>
2025-07-31 03:09:18 +0300

    [feat] service: edits in  news service

--- src/app/features/dashboard/modules/admin/dashboard/components/news/services/news.service.ts
+++ src/app/features/dashboard/modules/admin/dashboard/components/news/services/news.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { NewsItem } from '../interfaces/news-item.interface';
 
 @Injectable({
@@ -7,17 +8,22 @@ import { NewsItem } from '../interfaces/news-item.interface';
 })
 export class NewsService {
   private apiUrl = 'http://localhost:3000/news';
+
   constructor(private http: HttpClient) {}
-  getNews() {
+
+  getNews(): Observable<NewsItem[]> {
     return this.http.get<NewsItem[]>(this.apiUrl);
   }
-  addNews(news: NewsItem) {
+
+  addNews(news: NewsItem): Observable<NewsItem> {
     return this.http.post<NewsItem>(this.apiUrl, news);
   }
-  editNews(news: NewsItem) {
-    return this.http.put<NewsItem>(`${this.apiUrl}/${news.id}`, news);
+
+  editNews(id: number, news: NewsItem): Observable<NewsItem> {
+    return this.http.put<NewsItem>(`${this.apiUrl}/${id}`, news);
   }
-  deleteNews(id: number) {
+
+  deleteNews(id: number): Observable<NewsItem> {
     return this.http.delete<NewsItem>(`${this.apiUrl}/${id}`);
   }
 }
