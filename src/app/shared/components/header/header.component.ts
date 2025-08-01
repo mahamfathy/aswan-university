16b354aead970c1f1e7f5b2e4282f5d441a6aae2
mahamfathy <mahafathyh@gmail.com>
2025-08-02 00:15:02 +0300

    [feat] component:  finish header component

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
-
+interface NavLink {
+  label: string;
+  route: string;
+  hasDropdown?: boolean;
+  dropdownItems?: { label: string; route: string }[];
+}
 @Component({
   selector: 'app-header',
   standalone: false,
@@ -9,4 +14,33 @@ import { Component } from '@angular/core';
 export class HeaderComponent {
   isMenuOpen: boolean = false;
   isMobileMenuOpen: boolean = false;
+
+  navLinks: NavLink[] = [
+    { label: 'الرئيسية', route: '/home' },
+    { label: 'عن الجامعة', route: '/about' },
+    { label: 'الأقسام', route: '/departments' },
+    { label: 'الكليات والبرامج', route: '/faculties' },
+    { label: 'البحث العلمي', route: '/search' },
+    { label: 'الخدمات', route: '/services' },
+    { label: 'الأخبار', route: '/news' },
+    { label: 'الحياة الجامعية', route: '/life' },
+    { label: 'القبول', route: '/admissions' },
+    { label: 'تواصل معنا', route: '/contact' },
+    {
+      label: 'البوابة الطلابية',
+      route: '/portal',
+      hasDropdown: true,
+      dropdownItems: [
+        { label: 'خدمات طلابيه', route: '/portal/student-services' },
+        {
+          label: 'خدمات أعضاء هيئة التدريس',
+          route: '/portal/faculty-services',
+        },
+        { label: 'خدمات الإداريين', route: '/portal/admin-services' },
+        { label: 'الخدمات الطبيه', route: '/portal/medical-services' },
+        { label: 'خدمات ولي الأمر', route: '/portal/parent-services' },
+        { label: 'خدمات الخريجين', route: '/portal/graduates-services' },
+      ],
+    },
+  ];
 }
