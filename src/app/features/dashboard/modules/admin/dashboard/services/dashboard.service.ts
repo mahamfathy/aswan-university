473bcf6ba57f6164eb656d01b0063d8d0caa877a
mahamfathy <mahafathyh@gmail.com>
2025-08-02 02:00:45 +0300

    [feat] services: edits in services

--- src/app/features/dashboard/modules/admin/dashboard/services/dashboard.service.ts
+++ src/app/features/dashboard/modules/admin/dashboard/services/dashboard.service.ts
@@ -3,9 +3,4 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root',
 })
-export class DashboardService {
-  // constructor(private _http: HttpClient) {}
-  // changePassword(data: IChangePassword) {
-  //   return this._http.post('admin/users/change-password', data);
-  // }
-}
+export class DashboardService {}
